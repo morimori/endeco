f938cabe5b07c4a6b31664368a94c49dccd99803
森 一彦 <mori@mbp.local>
2012-01-17 15:16:21 +0900

    modify to use in application.rb.

--- lib/endeco/rails.rb
+++ lib/endeco/rails.rb
@@ -4,7 +4,7 @@ module Endeco
   class Railtie < Rails::Railtie
     config.endeco = ActiveSupport::OrderedOptions.new
 
-    config.before_initialize do
+    config.before_configuration do
       Endeco::Config.path  = config.endeco.path         || "#{Rails.root}/env"
       Endeco::Config.env   = config.endeco.env          || Rails.env.to_s
       Endeco::Cache.enable = config.endeco.enable_cache || Rails.env.production?
